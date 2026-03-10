4060b6eb4eec4cd94e8860c1f3e5abbdc252f5b7
AlvaroGC03 <guzcaalvaro@gmail.com>
2026-03-10 00:49:37 -0600

    feat: Limpieza de codigo y comentarios

--- rust/src/bin/coordinator.rs
+++ rust/src/bin/coordinator.rs
@@ -1,60 +1,64 @@
-use std::collections::HashMap; // Cambiamos HashSet por HashMap
-use std::sync::Arc;
-use std::time::{Duration, Instant};
-use tokio::sync::Mutex;
-use tonic::{transport::Server, Request, Response, Status};
+//importacion de librerias
+use std::collections::HashMap; //Diccionario
+use std::sync::Arc; //Atomic Reference Counted pointer (compartir datos inmutables entre multiples hilos)
+use std::time::{Duration, Instant}; //Tiempo
+use tokio::sync::Mutex; //Bloque mutuo
+use tonic::{transport::Server, Request, Response, Status}; //Elementos gRPC
 
-use mandelbrot_distribuido::mandelbrot::calculator_server::{Calculator, CalculatorServer};
-use mandelbrot_distribuido::mandelbrot::{Empty, ResultPayload, Task};
+//Importacionn de .proto
+use mandelbrot_distribuido::mandelbrot::calculator_server::{Calculator, CalculatorServer}; //trait del servicio gRPC
+use mandelbrot_distribuido::mandelbrot::{Empty, ResultPayload, Task}; //Mensajes gRPC
 
+//Estructura del estado global
 #[derive(Debug)]
 struct CoordinatorState {
-    pending_tasks: Vec<u32>,
-    in_progress: HashMap<u32, Instant>, // NUEVO: Tareas prestadas y la hora en que se prestaron
-    width: u32,
-    height: u32,
-    max_iterations: u32,
-    completed_tasks: u32,
-    start_time: Instant,
-    image_buffer: Vec<u8>,
-    worker_heartbeats: HashMap<String, Instant>, // NUEVO: Registro de "última vez visto" por worker
+    pending_tasks: Vec<u32>, //Cola de tareas pendientes
+    in_progress: HashMap<u32, Instant>, //Tareas actualmente en calculo (task_id -> tiempo en que se asigno)
+    width: u32, //Ancho de imagen
+    height: u32,//Alto de imagen
+    max_iterations: u32, //Numero maximo de iteraciones para calcular Mandelbrot.
+    completed_tasks: u32, //Numero de filas ya calculadas
+    start_time: Instant, //Momento en el que comenzo el calculo
+    image_buffer: Vec<u8>, //Arreglo donde se guardan todos los pixeles de la imagen final.
+    worker_heartbeats: HashMap<String, Instant>, //Control de vida de worker (worker_id -> ultima vez que reporto)
 }
 
+//Estructura de Coordinador
 #[derive(Debug)]
 struct MyCoordinator {
-    state: Arc<Mutex<CoordinatorState>>,
+    state: Arc<Mutex<CoordinatorState>>, //Estado compartido que permite acceso concurrente y seguridad en multiples request
 }
 
+//Implementacion del servicio gRPC
 #[tonic::async_trait]
 impl Calculator for MyCoordinator {
-    async fn get_task(&self, _request: Request<Empty>) -> Result<Response<Task>, Status> {
-        let mut state = self.state.lock().await;
-        let now = Instant::now();
-        let timeout = Duration::from_secs(15); // Si un worker tarda más de 15s en una fila, lo damos por muerto
+    async fn get_task(&self, _request: Request<Empty>) -> Result<Response<Task>, Status> { //Funcion get_task de .proto (worker solicita trabajo)
+        let mut state = self.state.lock().await; //Bloqueo de estado para su modificacion
+        let now = Instant::now(); //Obtener tiempo actual
+        let timeout = Duration::from_secs(15); //Tiempo maximo de respueta de worker
 
-        // 1. REVISIÓN DE CAÍDAS: Buscamos si alguna tarea en progreso expiró
-        let mut expired_task = None;
-        for (&task_id, &start_time) in state.in_progress.iter() {
-            if now.duration_since(start_time) > timeout {
-                expired_task = Some(task_id);
-                break; // Encontramos una, la rescatamos
+        //Deteccion de workers caidos
+        let mut expired_task = None; //Variable donde se guarda una tarea perdida
+        for (&task_id, &start_time) in state.in_progress.iter() { //Iteracion sobre tareas en progreso
+            if now.duration_since(start_time) > timeout { //Si se supera el tiempo maximo de respuesta
+                expired_task = Some(task_id); //Se marca la tarea como expirada y se guarda en variable
+                break; 
             }
         }
 
-        // 2. ASIGNACIÓN: Decidimos qué tarea darle al worker
-        let task_to_assign = if let Some(id) = expired_task {
+        //Asignacion de tarea a worker
+        let task_to_assign = if let Some(id) = expired_task { //Si encontramos una tarea expirada
             println!(">> ¡Worker caído detectado! Reasignando fila perdida: {}", id);
-            Some(id)
+            Some(id) //Reasignamos tarea
         } else {
             // Si no hay tareas expiradas, sacamos una nueva de la cola normal
             state.pending_tasks.pop()
         };
 
-        // 3. ENTREGAMOS LA TAREA
-        if let Some(row) = task_to_assign {
-            // La registramos como "En progreso" con la hora actual
-            state.in_progress.insert(row, Instant::now());
-            
+        //Entrega de tarea
+        if let Some(row) = task_to_assign { //Si existe una tarea disponible
+            state.in_progress.insert(row, Instant::now()); // La registramos como "En progreso" con la hora actual
+            //Creamos la estructura de Task
             let task = Task {
                 task_id: row,
                 row,
@@ -62,63 +66,62 @@ impl Calculator for MyCoordinator {
                 height: state.height,
                 max_iterations: state.max_iterations,
             };
-            Ok(Response::new(task))
+            Ok(Response::new(task)) //Enviamos respuesta gRPC
         } else {
             // Si no hay tareas nuevas ni expiradas, verificamos si ya terminamos todo
             if state.in_progress.is_empty() {
                 Err(Status::not_found("¡Fractal completado!"))
             } else {
-                // Hay tareas calculándose, pero aún no expiran. Le decimos al worker que espere.
+                // Hay tareas calculandose, pero aun no expiran. Le decimos al worker que espere.
                 Err(Status::unavailable("Esperando a que los demás terminen sus tareas..."))
             }
         }
     }
-
+    
+    //Funcion para que el Worker entregue resultados
     async fn submit_result(
         &self,
         request: Request<ResultPayload>,
     ) -> Result<Response<Empty>, Status> {
-        let payload = request.into_inner();
-        let mut state = self.state.lock().await;
-        let now = Instant::now();
+        let payload = request.into_inner(); //Extrae el mensaje gRPC y obtiene Payload
+        let mut state = self.state.lock().await; //Bloquea estado
+        let now = Instant::now(); //Tiempo actual
 
-        // Actualizamos el "Latido de vida" de este worker
-        state.worker_heartbeats.insert(payload.worker_id.clone(), now);
+        state.worker_heartbeats.insert(payload.worker_id.clone(), now); //Actualiza el ultimo contacto del worker
 
-        // SOLO procesamos el resultado si la tarea sigue en nuestra lista "En progreso"
-        // (Esto evita que si un worker "revive" tarde, arruine el contador sumando doble)
+        // Validamos tarea - SOLO procesamos el resultado si la tarea sigue en nuestra lista "En progreso"
         if state.in_progress.remove(&payload.task_id).is_some() {
-            let start_idx = (payload.task_id * state.width) as usize;
-            let end_idx = start_idx + (state.width as usize);
+            let start_idx = (payload.task_id * state.width) as usize; //Inicio de la fila
+            let end_idx = start_idx + (state.width as usize); //Fin de la fila
 
             if payload.data.len() == state.width as usize {
-                state.image_buffer[start_idx..end_idx].copy_from_slice(&payload.data);
+                state.image_buffer[start_idx..end_idx].copy_from_slice(&payload.data); //Copia los pixeles calculados a image_buffer
             }
 
-            state.completed_tasks += 1;
+            state.completed_tasks += 1; //Actualiza el progreso
             
-            // LIMPIEZA DE WORKERS: Borramos del contador a los que llevan más de 15s sin reportarse
+            // Borra del contador a los que llevan mas de 15s sin reportarse
             let heartbeat_timeout = Duration::from_secs(15);
             state.worker_heartbeats.retain(|_, &mut last_seen| now.duration_since(last_seen) < heartbeat_timeout);
 
-            if state.completed_tasks % 100 == 0 || state.completed_tasks == state.height {
+            if state.completed_tasks % 100 == 0 || state.completed_tasks == state.height { //Cada 100 filas imprimimos
                 println!(
                     "Progreso: {}/{} | Workers activos reales: {}", 
                     state.completed_tasks, 
                     state.height,
-                    state.worker_heartbeats.len() // Ahora sí, el número será 100% exacto
+                    state.worker_heartbeats.len() 
                 );
             }
 
-            if state.completed_tasks == state.height {
-                let duration = state.start_time.elapsed();
+            if state.completed_tasks == state.height { //Si el progreso coincide con la altura de la imagen, se termino todo
+                let duration = state.start_time.elapsed(); //Medida de teimpo transcurrido
                 
                 println!("\n====================================================");
                 println!("¡CÁLCULO DISTRIBUIDO COMPLETADO!");
                 println!("Tiempo total de ejecución: {:.2?}", duration);
                 println!("====================================================\n");
 
-                if let Err(e) = image::save_buffer(
+                if let Err(e) = image::save_buffer( //Guardamos el buffer como imagen png
                     "mandelbrot.png",
                     &state.image_buffer,
                     state.width,
@@ -136,15 +139,18 @@ impl Calculator for MyCoordinator {
     }
 }
 
+//Funcion main
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "0.0.0.0:3000".parse().unwrap();
-    let width = 3840;
-    let height = 2160;
+    let addr = "0.0.0.0:3000".parse().unwrap(); //El servidor escuha el puesto 3000
+    //Definimos resolucion (dimensiones) de la imagen
+    let width = 1920;
+    let height = 1080;
     
-    let mut pending_tasks: Vec<u32> = (0..height).collect();
-    pending_tasks.reverse();
+    let mut pending_tasks: Vec<u32> = (0..height).collect(); //Crea la lista de tareas
+    pending_tasks.reverse(); //Se invierte para usar pop
 
+    //Inicializa todo el sistema
     let state = CoordinatorState {
         pending_tasks,
         in_progress: HashMap::new(), // Inicializamos vacío
@@ -153,19 +159,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         max_iterations: 1000,
         completed_tasks: 0,
         start_time: Instant::now(),
-        image_buffer: vec![0; (width * height) as usize],
+        image_buffer: vec![0; (width * height) as usize], //Inicializamos vector lleno de ceros
         worker_heartbeats: HashMap::new(), // Inicializamos vacío
     };
 
+    //Estado compartido entre requests
     let coordinator = MyCoordinator {
         state: Arc::new(Mutex::new(state)),
     };
 
     println!("Coordinador escuchando en {}", addr);
 
-    Server::builder()
-        .add_service(CalculatorServer::new(coordinator))
-        .serve(addr)
+    Server::builder() //Crea el servidor
+        .add_service(CalculatorServer::new(coordinator)) //Resgitra el servicio gRPC
+        .serve(addr) //Inicia el servidor
         .await?;
 
     Ok(())
--- rust/src/bin/worker.rs
+++ rust/src/bin/worker.rs
@@ -1,14 +1,15 @@
-use std::time::Duration;
-use tonic::Request;
+//Importacion de librerias
+use std::time::Duration; //Tiempo
+use tonic::Request; //Estrcutura de peticion de tonic
 
-// Importamos el código gRPC generado (la versión Cliente)
-use mandelbrot_distribuido::mandelbrot::calculator_client::CalculatorClient;
-use mandelbrot_distribuido::mandelbrot::{Empty, ResultPayload};
+// Importaciones de .proto
+use mandelbrot_distribuido::mandelbrot::calculator_client::CalculatorClient; //Cliente gRPC
+use mandelbrot_distribuido::mandelbrot::{Empty, ResultPayload}; //Mensajes gRPC
 
-// --- EL MOTOR MATEMÁTICO ---
-// Esta función toma el número de fila y calcula el color de todos sus píxeles.
+// Motor matematico
+// Calculo de una fila completa de mandelbrot
 fn calculate_mandelbrot_row(row: u32, width: u32, height: u32, max_iterations: u32) -> Vec<u8> {
-    let mut pixels = Vec::with_capacity(width as usize);
+    let mut pixels = Vec::with_capacity(width as usize); //Crea buffer de pixeles
 
     // Límites matemáticos del fractal en el plano cartesiano
     let x_min = -2.0;
@@ -16,57 +17,61 @@ fn calculate_mandelbrot_row(row: u32, width: u32, height: u32, max_iterations: u
     let y_min = -1.5;
     let y_max = 1.5;
 
-    // Convertimos nuestra "fila" de la imagen en una coordenada Y del plano cartesiano
+    // Convierte fila de imagen en una coordenada Y del plano cartesiano
     let y0 = y_min + (row as f64 / height as f64) * (y_max - y_min);
 
-    for col in 0..width {
-        // Convertimos el "píxel" actual en una coordenada X del plano cartesiano
-        let x0 = x_min + (col as f64 / width as f64) * (x_max - x_min);
+    for col in 0..width { //Itera sobre todas las columnas de la fila
+        let x0 = x_min + (col as f64 / width as f64) * (x_max - x_min); // Convierte el pixel actual en una coordenada X del plano cartesiano
 
+        //Incializa variables de fractal
         let mut x = 0.0;
         let mut y = 0.0;
         let mut iteration = 0;
 
-        // La famosa fórmula: Z = Z^2 + C
-        while x * x + y * y <= 4.0 && iteration < max_iterations {
+        // Formula: Z = Z^2 + C - Criterio de escape
+        while x * x + y * y <= 4.0 && iteration < max_iterations { //Si la magnitud supera 4 = el punto escapa al infinito y no pertenece
+            //Implementacion de la ecuacion
             let x_temp = x * x - y * y + x0;
             y = 2.0 * x * y + y0;
             x = x_temp;
-            iteration += 1;
+            iteration += 1; //Conteo de iteraciones
         }
 
-        // Convertimos las iteraciones en un valor de "color" (un byte de 0 a 255)
+        // Convierte las iteraciones en un valor de "color" (un byte de 0 a 255)
         let color_value = (iteration % 256) as u8;
-        pixels.push(color_value);
+        pixels.push(color_value); //Guarda el pixel en el vector
     }
 
-    // Regresamos el arreglo de colores crudos (Bytes)
+    // Regresa el arreglo de colores crudos (Bytes) de la fila
     pixels
 }
 
+//Funcion main
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // IMPORTANTE: Como estás en tu máquina local sin conectar a la VPN por ahora,
-    // usaremos 127.0.0.1. Cuando lo lleves a Docker, lo cambiarás por 10.0.0.1
-    let coordinator_url = "http://10.0.0.2:3000";
-    let unique_time = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
+    let coordinator_url = "http://10.0.0.2:3000"; //Apunta a la direccion del coordinador
+    //let coordinator_url = "http://127.0.0.1:3000"; //Apunta a la direccion del coordinador (local)
+    
+    let unique_time = std::time::SystemTime::now() //Obtiene el tiempo actual
+        .duration_since(std::time::UNIX_EPOCH) //Convierte
         .unwrap()
-        .subsec_nanos();
+        .subsec_nanos(); //Obtiene nanosegundos
         
-    let my_worker_id = format!("worker-{}", unique_time);
+    let my_worker_id = format!("worker-{}", unique_time); //Crea ID unico de worker con el tiempo (ej. worker-83726481)
 
+    //Mensajes de inicio
     println!("Iniciando worker: {}", my_worker_id);
     println!("Buscando al coordinador en {}...", coordinator_url);
 
-    // TOLERANCIA A FALLOS: Un bucle infinito intentando conectar.
-    // Si el coordinador se cae o aún no está levantado, el worker no muere, solo espera.
+    // Un bucle infinito intentando conectar.
     let mut client = loop {
-        match CalculatorClient::connect(coordinator_url).await {
+        match CalculatorClient::connect(coordinator_url).await { //Intento de conexion
+            //Si conecta correctamente rompe bucle
             Ok(c) => {
                 println!("¡Conexión gRPC establecida exitosamente!\n");
                 break c;
             }
+            //Si falla espera 3 seg. y vuelve a intentar
             Err(_) => {
                 println!("Esperando al coordinador... Reintentando en 3s...");
                 tokio::time::sleep(Duration::from_secs(3)).await;
@@ -74,17 +79,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
-    // EL BUCLE DE TRABAJO
+    //Bucle principal de trabajo
     loop {
-        // 1. Pedimos trabajo al coordinador
-        let request = Request::new(Empty {});
+        let request = Request::new(Empty {}); //Crea una peticion de tarea vacia
         
-        match client.get_task(request).await {
-            Ok(response) => {
-                let task = response.into_inner();
-                // println!(">> Recibida Tarea: Calcular fila {}", task.row);
+        match client.get_task(request).await { //Lama remotamente al coordinador
+            //Si recibe tarea
+            Ok(response) => { //El coordinador asigno tarea
+                let task = response.into_inner(); //Extrae Tarea
+                 println!(">> Tarea Recibida: Calcular fila {}", task.row);
 
-                // 2. Ejecutamos el motor matemático (El trabajo pesado real)
+                //ejecuta calculo de fractal
                 let pixels_data = calculate_mandelbrot_row(
                     task.row,
                     task.width,
@@ -92,31 +97,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     task.max_iterations,
                 );
 
-                // 3. Empaquetamos los bytes binarios
-                let result_payload = ResultPayload {
+                //Empaqueta los bytes binarios
+                let result_payload = ResultPayload { //Crea payload de resultado
                     task_id: task.task_id,
                     worker_id: my_worker_id.to_string(),
                     data: pixels_data,
                 };
 
-                // 4. Enviamos el resultado por el túnel gRPC
+                // Envia el resultado por el tunel gRPC
                 let result_req = Request::new(result_payload);
-                if let Err(e) = client.submit_result(result_req).await {
+                if let Err(e) = client.submit_result(result_req).await { //Si falla el envio
                     println!("Error enviando resultado al coordinador: {:?}", e);
-                } else {
+                } else { //Si funciona
                     println!("<< Fila {} calculada y enviada", task.row);
                 }
             }
-            Err(status) => {
-                // Si el coordinador responde "NOT_FOUND", significa que ya se calculó toda la imagen
-                if status.code() == tonic::Code::NotFound {
+            Err(status) => { //Si no hay tareas  
+                if status.code() == tonic::Code::NotFound { // Si el coordinador responde "NOT_FOUND", significa que ya se calculo toda la imagen
                     println!("\n¡El coordinador informa que el fractal está completo!");
                     println!("Worker en reposo esperando nuevas órdenes...");
                     tokio::time::sleep(Duration::from_secs(5)).await;
-                } else {
+                } else { //Si se perdio conexion
                     println!("Se perdió la conexión con el coordinador: {:?}", status);
                     tokio::time::sleep(Duration::from_secs(3)).await;
-                    // Aquí podrías agregar lógica para intentar reconectar
+                    //Logica de reconexion por implementar
                 }
             }
         }
